12ba6aeaee5e24352eaea28aa887d73fe705c484
Hayden Keppel <106423952+haydenkeppel@users.noreply.github.com>
2024-02-08 12:17:05 +1300

    Establish

---
+++ app.js
@@ -0,0 +1,493 @@
+const timetable = {
+    "1": {
+        "850": {
+            "time": "8:50 am",
+            "period": "Period 1",
+            "code": "3PHY",
+            "class": "Physics",
+            "teacher": "SPL",
+            "room": "L4B"
+        },
+        "950": {
+            "time": "9:50 am",
+            "period": "Period 2",
+            "code": "3CAL",
+            "class": "Calculus",
+            "teacher": "JWR",
+            "room": "14"
+        },
+        "1050": {
+            "time": "10:50 am",
+            "period": "Tutor",
+            "code": "Tutor",
+            "class": "Tutor",
+            "teacher": "GSD",
+            "room": "ARTCOM"
+        },
+        "1110": {
+            "time": "11:10 am",
+            "period": "Interval",
+            "code": "",
+            "class": "Interval",
+            "teacher": "",
+            "room": ""
+        },
+        "1135": {
+            "time": "11:35 am",
+            "period": "Period 3",
+            "code": "3CSC",
+            "class": "Computer Science",
+            "teacher": "NCA",
+            "room": "TEC2"
+        },
+        "1235": {
+            "time": "12:35 pm",
+            "period": "Period 4",
+            "code": "Study",
+            "class": "Study Period",
+            "teacher": "",
+            "room": ""
+        },
+        "1335": {
+            "time": "1:35 pm",
+            "period": "Lunch",
+            "code": "",
+            "class": "Lunch",
+            "teacher": "",
+            "room": ""
+        },
+        "1415": {
+            "time": "2:15 pm",
+            "period": "Period 5",
+            "code": "3EMS",
+            "class": "Earth and Marine Science",
+            "teacher": "GBL",
+            "room": "L4B"
+        },
+        "1515": {
+            "time": "3:15 pm",
+            "period": "After School",
+            "code": "",
+            "class": "End of Day",
+            "teacher": "",
+            "room": ""
+        }
+    },
+    "2": {
+        "850": {
+            "time": "8:50 am",
+            "period": "Period 1",
+            "code": "3PSY",
+            "class": "Psychology",
+            "teacher": "ACN",
+            "room": "20"
+        },
+        "950": {
+            "time": "9:50 am",
+            "period": "Period 2",
+            "code": "3PHY",
+            "class": "Physics",
+            "teacher": "SPL",
+            "room": "L4B"
+        },
+        "1050": {
+            "time": "10:50 am",
+            "period": "Tutor",
+            "code": "Tutor",
+            "class": "Tutor",
+            "teacher": "GSD",
+            "room": "ARTCOM"
+        },
+        "1110": {
+            "time": "11:10 am",
+            "period": "Interval",
+            "code": "",
+            "class": "Interval",
+            "teacher": "",
+            "room": ""
+        },
+        "1135": {
+            "time": "11:35 am",
+            "period": "Period 3",
+            "code": "3CAL",
+            "class": "Calculus",
+            "teacher": "JWR",
+            "room": "14"
+        },
+        "1235": {
+            "time": "12:35 pm",
+            "period": "Period 4",
+            "code": "3EMS",
+            "class": "Earth and Marine Science",
+            "teacher": "GBL",
+            "room": "L4B"
+        },
+        "1335": {
+            "time": "3:15 pm",
+            "period": "After School",
+            "code": "",
+            "class": "End of Day",
+            "teacher": "",
+            "room": ""
+        }
+    },
+    "3": {
+        "850": {
+            "time": "8:50 am",
+            "period": "Period 1",
+            "code": "3EMS",
+            "class": "Earth and Marine Science",
+            "teacher": "GBL",
+            "room": "L4B"
+        },
+        "950": {
+            "time": "9:50 am",
+            "period": "Period 2",
+            "code": "3PSY",
+            "class": "Psychology",
+            "teacher": "ACN",
+            "room": "20"
+        },
+        "1050": {
+            "time": "10:50 am",
+            "period": "Tutor",
+            "code": "Tutor",
+            "class": "Tutor",
+            "teacher": "GSD",
+            "room": "ARTCOM"
+        },
+        "1110": {
+            "time": "11:10 am",
+            "period": "Interval",
+            "code": "",
+            "class": "Interval",
+            "teacher": "",
+            "room": ""
+        },
+        "1135": {
+            "time": "11:35 am",
+            "period": "Period 3",
+            "code": "Study",
+            "class": "Study Period",
+            "teacher": "",
+            "room": ""
+        },
+        "1235": {
+            "time": "12:35 pm",
+            "period": "Period 4",
+            "code": "3CAL",
+            "class": "Calculus",
+            "teacher": "JWR",
+            "room": "14"
+        },
+        "1335": {
+            "time": "1:35 pm",
+            "period": "Lunch",
+            "code": "",
+            "class": "Lunch",
+            "teacher": "",
+            "room": ""
+        },
+        "1415": {
+            "time": "2:15 pm",
+            "period": "Period 5",
+            "code": "3CSC",
+            "class": "Computer Science",
+            "teacher": "NCA",
+            "room": "TEC2"
+        },
+        "1515": {
+            "time": "3:15 pm",
+            "period": "After School",
+            "code": "",
+            "class": "End of Day",
+            "teacher": "",
+            "room": ""
+        }
+    },
+    "4": {
+        "1010": {
+            "time": "10:10 am",
+            "period": "Period 2",
+            "code": "3CAL",
+            "class": "Calculus",
+            "teacher": "JWR",
+            "room": "14"
+        },
+        "1110": {
+            "time": "11:10 am",
+            "period": "Interval",
+            "code": "",
+            "class": "Interval",
+            "teacher": "",
+            "room": ""
+        },
+        "1135": {
+            "time": "11:35 am",
+            "period": "Period 3",
+            "code": "3CSC",
+            "class": "Computer Science",
+            "teacher": "NCA",
+            "room": "TEC2"
+        },
+        "1235": {
+            "time": "12:35 pm",
+            "period": "Period 4",
+            "code": "3PSY",
+            "class": "Psychology",
+            "teacher": "ACN",
+            "room": "20"
+        },
+        "1335": {
+            "time": "1:35 pm",
+            "period": "Lunch",
+            "code": "",
+            "class": "Lunch",
+            "teacher": "",
+            "room": ""
+        },
+        "1415": {
+            "time": "2:15 pm",
+            "period": "Period 5",
+            "code": "3PHY",
+            "class": "Physics",
+            "teacher": "SPL",
+            "room": "L4B"
+        },
+        "1515": {
+            "time": "3:15 pm",
+            "period": "After School",
+            "code": "",
+            "class": "End of Day",
+            "teacher": "",
+            "room": ""
+        }
+    },
+    "5": {
+        "950": {
+            "time": "9:50 am",
+            "period": "Period 2",
+            "code": "3CSC",
+            "class": "Computer Science",
+            "teacher": "NCA",
+            "room": "TEC2"
+        },
+        "1050": {
+            "time": "10:50 am",
+            "period": "Tutor",
+            "code": "Tutor",
+            "class": "Tutor",
+            "teacher": "GSD",
+            "room": "ARTCOM"
+        },
+        "1110": {
+            "time": "11:10 am",
+            "period": "Interval",
+            "code": "",
+            "class": "Interval",
+            "teacher": "",
+            "room": ""
+        },
+        "1135": {
+            "time": "11:35 am",
+            "period": "Period 3",
+            "code": "3PHY",
+            "class": "Physics",
+            "teacher": "SPL",
+            "room": "L4B"
+        },
+        "1235": {
+            "time": "12:35 pm",
+            "period": "Period 4",
+            "code": "3EMS",
+            "class": "Earth and Marine Science",
+            "teacher": "GBL",
+            "room": "L4B"
+        },
+        "1335": {
+            "time": "1:35 pm",
+            "period": "Lunch",
+            "code": "",
+            "class": "Lunch",
+            "teacher": "",
+            "room": ""
+        },
+        "1415": {
+            "time": "2:15 pm",
+            "period": "Period 5",
+            "code": "3PSY",
+            "class": "Psychology",
+            "teacher": "ACN",
+            "room": "20"
+        },
+        "1515": {
+            "time": "3:15 pm",
+            "period": "After School",
+            "code": "",
+            "class": "End of Day",
+            "teacher": "",
+            "room": ""
+        }
+    }
+};
+
+const WEEKDAY_LIST = [
+    "Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"
+];
+const MONTH_LIST = [
+    "January", "February", "March", "April", 
+    "May", "June", "July", "August", "September", 
+    "October", "November", "December"
+];
+
+// TEMP:
+const WEEKDAY = new Date().getDay();
+// const WEEKDAY = 0;
+const DISPLAY_WEEKDAY = WEEKDAY_LIST[WEEKDAY];
+
+const DAY = new Date().getDate();
+let displayDay;
+if(DAY.toString().slice(-1) == 1 && DAY != 11) {
+    displayDay = `${DAY}st`;
+} else if(DAY.toString().slice(-1) == 2 && DAY != 12) {
+    displayDay = `${DAY}nd`;
+} else if(DAY.toString().slice(-1) == 3 && DAY != 13) {
+    displayDay = `${DAY}rd`;
+} else {
+    displayDay = `${DAY}th`;
+}
+
+const MONTH = new Date().getMonth();
+const DISPLAY_MONTH = MONTH_LIST[MONTH]
+
+const DISPLAY_DATE = `${DISPLAY_WEEKDAY}, ${displayDay} of ${DISPLAY_MONTH}`;
+document.getElementById("date").innerText = DISPLAY_DATE;
+
+
+let currentMinute = new Date().getMinutes();
+let displayMinute = currentMinute;
+if(currentMinute.toString().length < 2) {
+    displayMinute = `0${displayMinute}`;
+}
+
+let currentHour = new Date().getHours();
+let displayHour = currentHour;
+if(currentHour > 12) {
+    displayHour -= 12;
+}
+
+let suffix = "am";
+if(currentHour > 11) {
+    suffix = "pm";
+}
+
+let currentTime = `${displayHour}:${displayMinute} ${suffix}`;
+let timeValue = parseInt(`${currentHour}${displayMinute}`);
+
+let oldMinute = new Date().getMinutes();
+
+    function updateTime() {
+    let currentMinute = new Date().getMinutes();
+    let displayMinute = currentMinute;
+    if(currentMinute.toString().length < 2) {
+        displayMinute = `0${displayMinute}`;
+    }
+    
+    let currentHour = new Date().getHours();
+    let displayHour = currentHour;
+    if(currentHour > 12) {
+        displayHour -= 12;
+    }
+    
+    let suffix = "am";
+    if(currentHour > 11) {
+        suffix = "pm";
+    }
+    
+    let currentTime = `${displayHour}:${displayMinute} ${suffix}`;
+    document.getElementById("time").innerText = currentTime;
+    
+    let timeValue = parseInt(`${currentHour}${displayMinute}`);
+
+    if(oldMinute != currentMinute) {
+        oldMinute = currentMinute;
+        updatePeriod(timeValue);
+    }
+}
+
+var nextPeriod;
+var currentPeriod = {
+    "time": "",
+    "period": "",
+    "code": "",
+    "class": "Before School",
+    "teacher": "",
+    "room": ""
+};
+
+function updatePeriod(timeValue) {
+    document.getElementById("today").innerHTML = "";
+    for(time in timetable[WEEKDAY]) {
+        if(time > timeValue) {
+            nextPeriod = timetable[WEEKDAY][time];
+            break;
+        }
+        currentPeriod = timetable[WEEKDAY][time];
+    }
+    // console.log(currentPeriod, nextPeriod.class);
+    
+    const div_now = document.getElementById("now");
+    div_now.querySelector(".time").innerText = currentPeriod.time;
+    div_now.querySelector(".class").innerText = currentPeriod.class;
+    if(currentPeriod.teacher != "") {
+        div_now.querySelector(".teacher-room").innerText = `${currentPeriod.teacher} - ${currentPeriod.room}`;
+    } else {
+        div_now.querySelector(".teacher-room").innerText = "";
+    }
+    div_now.querySelector(".period").innerText = currentPeriod.period;
+    
+    const div_next = document.getElementById("next");
+    div_next.querySelector(".time").innerText = nextPeriod.time;
+    div_next.querySelector(".class").innerText = nextPeriod.class;
+    if(nextPeriod.teacher != "") {
+        div_next.querySelector(".teacher-room").innerText = `${nextPeriod.teacher} - ${nextPeriod.room}`;
+    } else {
+        div_next.querySelector(".teacher-room").innerText = "";
+    }
+    div_next.querySelector(".period").innerText = nextPeriod.period;
+    
+    // const div_today = document.getElementById("today");
+    const today = timetable[WEEKDAY];
+    for(periodNumber in today) {
+        const newDiv = document.createElement("div");
+        newDiv.className = "period-block";
+        if(today[periodNumber] == currentPeriod) {
+            newDiv.className += " current-period-highlight";
+        };
+    
+        const div_periodNum = document.createElement("div");
+        div_periodNum.className = "period-num";
+        div_periodNum.innerText = today[periodNumber].period;
+        newDiv.appendChild(div_periodNum);
+    
+        const div_details = document.createElement("div");
+        div_details.className = "details";
+        if(today[periodNumber].teacher != "") {
+            div_details.innerHTML = `
+            <span class="time">${today[periodNumber].time}</span>
+            <h3 class="class">${today[periodNumber].class}</h3>
+            <span class="teacher-room">${today[periodNumber].teacher} - ${today[periodNumber].room}</span>`;
+        } else {
+            div_details.innerHTML = `
+            <span class="time">${today[periodNumber].time}</span>
+            <h3 class="class">${today[periodNumber].class}</h3>
+            <span class="teacher-room"></span>`;
+        }
+        newDiv.appendChild(div_details);
+    
+        document.getElementById("today").appendChild(newDiv);
+    }
+}
+
+updateTime();
+updatePeriod(timeValue);
